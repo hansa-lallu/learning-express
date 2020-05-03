d19e4e45b2df55d2cdd1550eba87992c438d6c54
hansa-lallu <hansalallu@gmail.com>
2020-05-03 08:44:45 +0100

    handle requests to / and /users

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('first middleware')
-  next();
+// order of below is important if not using next otherwise. Have the more specific middleware first 
+
+app.use('/users', (req, res, next) => {
+  console.log('/users middleware')
+  res.send('<p>Middleware that handles just /users</p>')
 })
 
-app.use((req, res, next) => {
-  console.log('second middleware')
-  res.send('<p>Task almost completed</p>')
+app.use('/', (req, res, next) => {
+  console.log('/ middleware')
+  res.send('<p>Middleware that handles just /</p>')
 })
 
 app.listen(3000);
